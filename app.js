39118fb43d9ebc8c4d40aaec6f5c9541274d7700
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-02 17:01:55 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars');
+
 const PORT = 3000
 
 // 僅在非正式環境時, 使用 dotenv
@@ -10,16 +12,10 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
-
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+//建立名為hbs的樣版引擎並傳入exphbs的相關參數
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+//啟動樣版引擎hbs
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.send('hello world!')
