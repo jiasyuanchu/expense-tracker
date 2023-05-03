55b9d64448f527f7d3a3fe92861ae2193af14bc1
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-03 17:45:26 +0800

    feat: install body-parser

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,9 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const Category = require("../../models/category")
 
 router.get('/new', (req, res) => {
   return res.render('new')
+})
+
+router.post('/new', (req, res) => {
+  const { name, date, amount, categoryId } = req.body
+  return res.render('new')
+
 })
 
 module.exports = router
