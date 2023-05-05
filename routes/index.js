f9e4854b27e137c0b04ccf0e0d51a5064f76024a
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-05 11:28:45 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
 
 router.use('/records', records)
+router.use('/users', users)
 router.use('/', home)
 
 // 匯出路由器
